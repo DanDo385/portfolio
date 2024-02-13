01e7c22cec52ad9822100a1adcab4c3ec6b48165
Daniel Magro <dando385@yahoo.com>
2024-02-12 21:50:06 -0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import ThreeScene from '../components/ThreeScene';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Blockchain</title>
+      </Head>
+      <ThreeScene />
+    </>
+  );
+}
